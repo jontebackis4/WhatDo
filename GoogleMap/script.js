4c076991f079bd1f73f7b4426dc959308bb8f713
jontebackis4 <jonathan.backstrom95@gmail.com>
2016-03-30 11:42:42 +0200

    A bit better map

--- GoogleMap/script.js
+++ GoogleMap/script.js
@@ -1,19 +1,34 @@
 (function(window, google) {
   
 	//map options
-	//Sätt max zoom och min zoom
+	//Det går ändra så att det inte går att dra i kartan eller zooma med scrollern.
 
 	var options ={
 		center: {
-			lat: -34.834431,
-			lng: 138.688760
+			lat: 62.173276,
+			lng: 14.942265
 		},
-		zoom: 16
+		zoom: 5,
+		disableDefaultUI: false,
+		maxZoom: 7,
+		minZoom: 4,
+		zoomControl: true,
+		zoomControlOption: {
+			position: google.maps.ControlPosition.RIGHT_BOTTOM,
+			style: google.maps.ZoomControlStyle.Small
+		},
+		streetViewControl: false,
+		mapTypeControl: false
 	}
 
 	//map
-	// Lägg till en tagg så att sverige är regionen som arbetas med.
 
 	map = new google.maps.Map(document.getElementById('map-canvas'), options)
   
 }(window, google));
+
+/*
+	-Skapa en funktion som skapar marker på en viss lng&lat. Kolla events i guider.
+	-Det går att logga in med google-konto s¨att information sparas.
+	-Läser på singed in maps.
+*/
