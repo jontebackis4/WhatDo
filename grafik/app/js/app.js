e7461511d438bda229543c656c224d52f4e7a62c
jensbern <jensbern@kth.se>
2016-04-07 16:13:01 +0200

    hoppa mellan views

--- grafik/app/js/app.js
+++ grafik/app/js/app.js
@@ -8,7 +8,7 @@
 // also see that we included separate JavaScript files for these modules. Angular
 // has other core modules that you might want to use and explore when you go deeper
 // into developing Angular applications. For this lab, these two will suffice.
-var dinnerPlannerApp = angular.module('dinnerPlanner', ['ngRoute','ngResource']);
+var whatDoApp = angular.module('whatDo', ['ngRoute','ngResource']);
 //jdsgksd
 
 // Here we configure our application module and more specifically our $routeProvider. 
@@ -34,15 +34,15 @@ var dinnerPlannerApp = angular.module('dinnerPlanner', ['ngRoute','ngResource'])
 // the path we use the ":" sign. For instance, our '/dish/:dishId' will be triggered when we access 
 // 'http://localhost:8000/#/dish/12345'. The 12345 value will be stored in a dishId parameter, which we can
 // then access through $routeParams service. More information on this in the dishCtrl.js 
-dinnerPlannerApp.config(['$routeProvider',
+whatDoApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/home', {
         templateUrl: 'partials/home.html'
       }).
-      when('/search', {
-        templateUrl: 'partials/search.html',
-        controller: 'SearchCtrl'
+      when('/interests', {
+        templateUrl: 'partials/interests.html',
+        controller: 'interestsCtrl'
       }).
       when('/dish/:dishId', {
         templateUrl: 'partials/dish.html',
