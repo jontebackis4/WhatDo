ff0f6713956208d831250912ac21ff2ea60ded9e
jontebackis4 <jonathan.backstrom95@gmail.com>
2016-04-22 22:41:09 +0200

    Popup has correct information but needs style

--- grafik/app/js/modalInstanceCtrl.js
+++ grafik/app/js/modalInstanceCtrl.js
@@ -5,6 +5,10 @@ whatDoApp.controller('modalInstanceCtrl', ['$scope', '$uibModalInstance', 'WhatD
 	$scope.$on('update', function(){
 		$scope.info = WhatDo.selectedPlaceInfo;
 	});
+
+	$scope.getPhoto = function(photos){
+		return photos[0].getUrl({'maxWidth': photos[0].width, 'maxHeight': photos[0].height});
+	};
 	
 	$scope.close = function(){
 		$uibModalInstance.dismiss('close');
